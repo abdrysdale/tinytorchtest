3d4e7009d417d1cd502a4e074401e6cb03449212
Alex <adrysdale@protonmail.com>
2022-05-05 09:55:49 +0100

    Added single and multi argument regression model classes

--- test_networks.py
+++ test_networks.py
@@ -1,4 +1,4 @@
-"""Class for a neural network with multiple arguments."""
+"""Class for a test neural networks."""
 #! /usr/bin/env python
 
 # Python imports
@@ -7,9 +7,10 @@ import collections
 # Module imports
 import torch
 
-class multi_arg_nn(torch.nn.Module):
+class SingleArgRegression(torch.nn.Module):
+    """Single argument input (deep) neural network block"""
     def __init__(self, layers, **kwargs):
-        """Neural network class with multiple positional arguments
+        """Neural network class with a single positional arguments
 
         Args:
             layers (list) : A list of the size of each layer.
@@ -17,12 +18,11 @@ class multi_arg_nn(torch.nn.Module):
         Kwargs:
             activation (torch.nn activation function) : Module activation function.
                 Defaults to torch.nn.ReLU
-            device (str) : Device to store tensors. Defaults to "cpu".
         """
+        super().__init__()
 
         # Keyword arguments
         self.activation = kwargs.get("activation", torch.nn.ReLU)
-        device = kwargs.get("device", "cpu")
 
         # Parameters
         self.depth = len(layers) - 1
@@ -43,6 +43,28 @@ class multi_arg_nn(torch.nn.Module):
         self.layers = torch.nn.Sequential(layer_dict)
 
 
+    def forward(self, nn_input):
+        """Forward pass"""
+        return self.layers(nn_input)
+
+
+class MultiArgRegression(torch.nn.Module):
+    """Multiple argument input (deep) neural network block"""
+    def __init__(self, layers, **kwargs):
+        """Neural network class with multiple positional arguments
+
+        Args:
+            layers (list) : A list of the size of each layer.
+
+        Kwargs:
+            activation (torch.nn activation function) : Module activation function.
+                Defaults to torch.nn.ReLU
+            device (str) : Device to store tensors. Defaults to "cpu".
+        """
+        super().__init__()
+        self.layers = SingleArgRegression(layers, **kwargs)
+
     def forward(self, arg_1, arg_2):
+        """Forward pass"""
         nn_input = torch.cat((arg_1, arg_2))
         return self.layers(nn_input)
