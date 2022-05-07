2f585ea1b5ea361a83ef7ce8490461ef43afd29f
Alex <adrysdale@protonmail.com>
2022-05-07 08:59:38 +0100

    Tests for nan exception

--- tests/test_torchtest.py
+++ tests/test_torchtest.py
@@ -187,6 +187,41 @@ def test_params_dont_change():
             params=list(model.named_parameters()),
         )
 
+def test_nan_exception():
+
+    # Setup test suite
+    ttt.setup(1)
+
+    # Model
+    layers = [3, 10, 1]
+    model = test_networks.SingleArgRegression(layers)
+
+    # Data
+    data = [torch.rand(4, 3), torch.rand(4,1)]
+
+    data[0][0, 0] = float('nan')
+
+    # Optimiser
+    optim = torch.optim.Adam([p for p in model.parameters() if p.requires_grad])
+
+    # Loss
+    loss_fn = torch.nn.MSELoss()
+
+    # run all tests
+    with pytest.raises(ttt.NaNTensorException):
+        assert ttt.test_suite(
+            model,
+            loss_fn,
+            optim,
+            data,
+            train_vars=list(model.named_parameters()),
+            test_vars_change=True,
+            test_inf_vals=True,
+            test_nan_vals=True,
+        )
+
+
+
 if __name__ == '__main__':
     print("Running tests...")
     test_regression()
@@ -194,4 +229,5 @@ if __name__ == '__main__':
     test_regression_unsupervised()
     test_classification()
     test_params_dont_change()
+    test_nan_exception()
     print("Testing complete!")
