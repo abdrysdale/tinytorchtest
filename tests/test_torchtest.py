03cefb31c13e673dac85a539b91fd3fe4a88361b
alimongo <adrysdale@protonmail.com>
2022-10-25 11:12:21 +0100

    Fix/#7 unsupervised loss (#8)
    * Added better support for unsupervised learning
    
    * Updated tests
    
    * Updated README
    
    * Updated lock file

--- tests/test_torchtest.py
+++ tests/test_torchtest.py
@@ -94,8 +94,8 @@ def test_regression_unsupervised():
     optim = torch.optim.Adam([p for p in model.parameters() if p.requires_grad])
 
     # Loss
-    def _loss(output):
-        return torch.mean(output**2)
+    def _loss(output, input):
+        return torch.mean(output ** 2) + torch.mean(input[0] ** 2)
 
     # Setup test suite
     test = ttt.TinyTorchTest(model, _loss, optim, batch, supervised=False)
@@ -124,8 +124,8 @@ def test_multiarg_unsupervised():
     optim = torch.optim.Adam([p for p in model.parameters() if p.requires_grad])
 
     # Loss
-    def _loss(output):
-        return torch.mean(output**2)
+    def _loss(output, _):
+        return torch.mean(output ** 2)
 
     # Setup test suite
     test = ttt.TinyTorchTest(model, _loss, optim, batch, supervised=False)
